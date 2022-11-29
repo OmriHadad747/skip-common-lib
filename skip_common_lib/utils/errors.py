86a90583197b7e548a4bfae7912ed5cb8ba4d75e
OmriHadad747 <omrihadad747@gmail.com>
2022-11-29 20:47:26 +0200

    loggging

--- skip_common_lib/utils/errors.py
+++ skip_common_lib/utils/errors.py
@@ -1,3 +1,5 @@
+from logging import Logger
+
 from typing import Any
 from fastapi import status, HTTPException
 
@@ -22,7 +24,10 @@ class Errors:
         )
 
     @classmethod
-    def already_exist_freelancer_with_email(cls, email: str):
+    def already_exist_freelancer_with_email(cls, email: str, logger: Logger = None):
+        if logger:
+            logger.debug(f"already exist freelancer with mail: {email}")
+
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"already exist freelancer with mail: {email}",
@@ -36,19 +41,26 @@ class Errors:
 
     @classmethod
     def general_exception(cls, exc: Exception):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(exc))
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(exc))
 
     @classmethod
-    def db_op_not_acknowledged(cls, obj: Any, op: str):
+    def db_op_not_acknowledged(cls, obj: Any, op: str, logger: Logger = None):
+        if logger:
+            logger.error(f"db operation {op.upper()} on {obj} doesn't acknowledged")
+
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"db operation {op.upper()} on {obj} doesn't acknowledged",
         )
 
     @classmethod
-    def id_not_found(cls, id: str):
+    def id_not_found(cls, id: str, logger: Logger = None):
+        if logger:
+            logger.error(f"{id} not found")
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"{id} not found")
 
     @classmethod
-    def email_not_found(cls, email: str):
+    def email_not_found(cls, email: str, logger: Logger = None):
+        if logger:
+            logger.error(f"{email} not found")
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"{email} not found")
