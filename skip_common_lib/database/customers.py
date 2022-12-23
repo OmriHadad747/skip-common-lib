bd1f629952eec44ccf459c9559313f8b79652a92
OmriHadad747 <omrihadad747@gmail.com>
2022-12-23 14:21:49 +0200

    fix db asyncio

--- skip_common_lib/database/customers.py
+++ skip_common_lib/database/customers.py
@@ -25,81 +25,81 @@ class CustomerDB:
 
         return db[_customers].with_options(codec_options=codec_options)
 
-    @staticmethod
-    def _build_array_update_write(field: str, op: str, data: list[Any]) -> dict[str, Any] | None:
-        """
-        - building 'update' argument for PyMongo's UpdateOne method
-        for document fields that are an array type
+    # @staticmethod
+    # def _build_array_update_write(field: str, op: str, data: list[Any]) -> dict[str, Any] | None:
+    #     """
+    #     - building 'update' argument for PyMongo's UpdateOne method
+    #     for document fields that are an array type
 
-        Args:
-            - field (str): document's field name to update
-            - op (str): operation to perform. available options: ['add', 'rem']
-            - data (List[Any]): data to add or remove from the document's field
+    #     Args:
+    #         - field (str): document's field name to update
+    #         - op (str): operation to perform. available options: ['add', 'rem']
+    #         - data (List[Any]): data to add or remove from the document's field
 
-        Raises:
-            - ValueError: in case of unknown/unsupported 'op' (operation)
+    #     Raises:
+    #         - ValueError: in case of unknown/unsupported 'op' (operation)
 
-        Returns:
-            - dict[str, Any] | None: dictionary that can be rendered by MongoDB driver
-        """
-        if op == "add":
-            return {"$addToSet": {field: {"$each": data}}}
-        elif op == "rem":
-            return {"$pull": {field: {"$in": data}}}
-        else:
-            raise ValueError(f"unknown operation - {op}")
+    #     Returns:
+    #         - dict[str, Any] | None: dictionary that can be rendered by MongoDB driver
+    #     """
+    #     if op == "add":
+    #         return {"$addToSet": {field: {"$each": data}}}
+    #     elif op == "rem":
+    #         return {"$pull": {field: {"$in": data}}}
+    #     else:
+    #         raise ValueError(f"unknown operation - {op}")
 
-    @staticmethod
-    def _adapt_for_bulkwrite(
-        fields_to_update: dict[str, Any], _filter: dict[str, Any]
-    ) -> list[dict[str, Any]] | None:
-        """
-        - iterate through all the provided document fields and build a list of updates
-        to perform using the 'bulk_write' method
+    # @staticmethod
+    # def _adapt_for_bulkwrite(
+    #     fields_to_update: dict[str, Any], _filter: dict[str, Any]
+    # ) -> list[dict[str, Any]] | None:
+    #     """
+    #     - iterate through all the provided document fields and build a list of updates
+    #     to perform using the 'bulk_write' method
 
-        - in case a field is an array type, use '_build_array_update_write' static function
-        to create an 'update' object appropriate for an array
+    #     - in case a field is an array type, use '_build_array_update_write' static function
+    #     to create an 'update' object appropriate for an array
 
-        Args:
-            - fields_to_update (Dict[str, Any]): bunch of fields to update in a document
-            - _filter (Dict[str, Any]): filter to apply on the updates
+    #     Args:
+    #         - fields_to_update (Dict[str, Any]): bunch of fields to update in a document
+    #         - _filter (Dict[str, Any]): filter to apply on the updates
 
-        Raises:
-            - ValueError:
-                - in case an array field doesn't have kind of operation ['add', 'rem'] to perform
-                - in case an array field contains more then 1 operation ['add', 'rem'] to perform
+    #     Raises:
+    #         - ValueError:
+    #             - in case an array field doesn't have kind of operation ['add', 'rem'] to perform
+    #             - in case an array field contains more then 1 operation ['add', 'rem'] to perform
 
-        Returns:
-            - list[dict[str, Any]] | None: list of updateOne operation to perform as part of the 'bulk_write' method
-        """
-        writes = []
+    #     Returns:
+    #         - list[dict[str, Any]] | None: list of updateOne operation to perform as part of the 'bulk_write' method
+    #     """
+    #     writes = []
 
-        basic_update_write = {"$set": {}}
+    #     basic_update_write = {"$set": {}}
 
-        for field, val in fields_to_update.items():
-            if isinstance(val, dict):
-                if len(val) == 0 or len(val) > 1:
-                    raise ValueError(
-                        f"'{field}' can't be empty or contain more then 1 key/operation ['add', 'rem']"
-                    )
+    #     for field, val in fields_to_update.items():
+    #         if isinstance(val, dict):
+    #             if len(val) == 0 or len(val) > 1:
+    #                 raise ValueError(
+    #                     f"'{field}' can't be empty or contain more then 1 key/operation ['add', 'rem']"
+    #                 )
 
-                op, data = val.popitem()
-                writes.append(
-                    UpdateOne(
-                        _filter,
-                        CustomerDB._build_array_update_write(field, op, data),
-                    )
-                )
-            else:
-                basic_update_write["$set"][field] = val
+    #             op, data = val.popitem()
+    #             writes.append(
+    #                 UpdateOne(
+    #                     _filter,
+    #                     CustomerDB._build_array_update_write(field, op, data),
+    #                 )
+    #             )
+    #         else:
+    #             basic_update_write["$set"][field] = val
 
-        # append the '$set' update operation at the end of the list so it will be
-        # executed last, such that in case the user's email is a part of the
-        # update, filtering by user's email will not work (cause the user's email changed)
-        if len(basic_update_write.get("$set")) > 0:
-            writes.append(UpdateOne(_filter, basic_update_write))
+    #     # append the '$set' update operation at the end of the list so it will be
+    #     # executed last, such that in case the user's email is a part of the
+    #     # update, filtering by user's email will not work (cause the user's email changed)
+    #     if len(basic_update_write.get("$set")) > 0:
+    #         writes.append(UpdateOne(_filter, basic_update_write))
 
-        return writes
+    #     return writes
 
     @classmethod
     async def get_customer_by_id(cls, id: str) -> Any | None:
